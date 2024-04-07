46dd3b849a1c9d12274d627148e8533d3e12c1bc
Jenilambawala <76586953+Jenilambawala@users.noreply.github.com>
2024-04-07 23:16:26 +0530

    Jenil Changes

--- src/routes/menu/MenuCategories.js
+++ src/routes/menu/MenuCategories.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import SearchIcon from "../../assets/images/search-icon.png";
 import ResetLocation from "../../helpers/ResetLocation";
+import axios from "axios";
 
 const MenuCategories = ({ allCategories, changeCategory, resetPagination, findMenuItem }) => {
+  
+  const [categories,setCategories] = useState([]);
+  const fetchCategories = async () => {
+  const result = await axios.get("http://localhost:3000/admin/category/getCategories?page=1&limit=20",{
+      headers:{
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjA1ODVjNDNiNzcyZThiMjNjZDhkMGEiLCJpYXQiOjE3MTE2Mzc5NTZ9.v0rOymDaLNk-w9oZigZylgf_vyNykQZq9Nagi8ezXt0"
+      }
+    });
+    if(result.status===200) {
+      setCategories(result?.data?.categories);
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories();
+  },[])
+
+  
   return (
     <article className="side-menu">
       <section className="menu-search-section">
@@ -16,7 +35,8 @@ const MenuCategories = ({ allCategories, changeCategory, resetPagination, findMe
         <img src={SearchIcon} alt="" aria-hidden="true" className="menu-search-icon" />
       </section>
       <ul>
-        {allCategories.map((category) => (
+
+      {categories.map((category) => (
           <li key={category.id}>
             <NavLink
               to="/menu"
@@ -30,6 +50,27 @@ const MenuCategories = ({ allCategories, changeCategory, resetPagination, findMe
             </NavLink>
           </li>
         ))}
+
+
+
+        {/* Old Categories Start */}
+        {/* {allCategories.map((category) => (
+          <li key={category.id}>
+            <NavLink
+              to="/menu"
+              onClick={() => {
+                changeCategory(category.name);
+                ResetLocation();
+                resetPagination();
+              }}
+            >
+              {category.name}
+            </NavLink>
+          </li>
+        ))} */}
+        {/* Old Categories End */}
+
+
       </ul>
     </article>
   );
