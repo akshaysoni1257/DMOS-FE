fd1f351470a55f99b0919f3ac48567b3e6d3e6bf
Jenilambawala <76586953+Jenilambawala@users.noreply.github.com>
2024-04-18 23:11:17 +0530

    Jenil - Changes

--- src/components/login/LoginModal.js
+++ src/components/login/LoginModal.js
@@ -3,91 +3,48 @@ import "./loginModal.css";
 import LinkButton from "../Button";
 import { useNavigate } from "react-router-dom";
 import validateForm from "../validateForm";
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // dummymodel
 const LoginModal = ({ setLoginModalWindow, setValidLogin, loginModalWindow, hideMenu, validLogin, getUser }) => {
-  const navigate = useNavigate();
-  const [formValue, setFormValue] = useState({ email: '', password: '' });
-  const [formError, setFormError] = useState({});
-  const [submit, setSubmit] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [verificationError, setVerificationError] = useState('');
-  const validate = validateForm("login");
+  const [formData, setFormData] = useState({
+    email:"",
+    password:""
+  })
 
-  const getUsers = async () => {
-    try {
-      const response = await fetch(process.env.REACT_APP_USERS_URL);
-      const body = await response.json();
-      return body.data;
-    }
-    catch (err) {
-      console.log(err.message)
-    }
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]:value
+    })
   }
-
-
-  const handleValidation = (e) => {
-
-    const { name, value } = e.target;
-    setFormValue((prevFormValue) => ({
-      ...prevFormValue,
-      [name]: value,
-    }));
-  };
+  const handleSubmited = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:3001/user/customerLogin",formData);
+    if(response.status === 200) {
+      toast("Login success!");
+      setFormData({
+        email:"",
+        password:""
+      })
+      setValidLogin(true)
+      hideLoginModal();
+    } else {
+      setValidLogin(false)
+    }
+    console.log(response);
+  }
 
   const hideLoginModal = () => {
     setLoginModalWindow(false);
-    setFormValue({ email: '', password: '' });
-    setFormError({});
-    setSubmit(false);
   }
 
-  const handleLogin = async (e) => {
-    setVerificationError('');
-    e.preventDefault();
-    setLoading(true);
-    setFormError(validate(formValue));
-    if (Object.keys(validate(formValue)).length > 0) {
-      setLoading(false);
-      return;
-    }
-    else {
-      //find all users
-      const existingUsers = await getUsers(formValue.email.toLowerCase());
-      //filter existence by email
-      const findByEmail = existingUsers.filter((u) => u.email === formValue.email.toLowerCase());
-      // if user not found by email
-      if (findByEmail.length === 0) {
-        setLoading(false);
-        setSubmit(false);
-        setFormValue({ email: '', password: '' });
-        setFormError({})
-        setVerificationError("Wrong email");
-        return;
-      }
-      else if (findByEmail.length > 0 && findByEmail[0].password !== formValue.password) {
-        setLoading(false);
-        setSubmit(false);
-        setFormValue({ email: '', password: '' });
-        setFormError({});
-        setVerificationError("Wrong password");
-        return;
-      }
-      else if (findByEmail.length > 0 && findByEmail[0].password === formValue.password) {
-        getUser(findByEmail[0].id);
-        setLoading(false);
-        hideLoginModal();
-        setFormValue({ email: '', password: '' });
-        setFormError({});
-        setVerificationError("");
-        setValidLogin(true);
-        navigate('/menu');
-      }
-    }
-  };
-
-
   return (
+    <>
+    <ToastContainer />
     <article className={`modal ${loginModalWindow ? 'active-modal' : null}`}>
       <section className="modal-main">
         <button
@@ -95,27 +52,24 @@ const LoginModal = ({ setLoginModalWindow, setValidLogin, loginModalWindow, hide
           type="button"
           onClick={() => {
             hideLoginModal();
-
           }}
         >
           X
         </button>
         <section className="modal-content">
           <h2>Log in</h2>
-          {loading ?
-            <div role="status" className="loader">
-              <p>Almost there...</p>
-              <img alt="Processing request" src="https://media0.giphy.com/media/L05HgB2h6qICDs5Sms/giphy.gif?cid=ecf05e472hf2wk1f2jou3s5fcnx1vek6ggnfcvhsjbeh7v5u&ep=v1_stickers_search&rid=giphy.gif&ct=s" />
-            </div> :
-            <form onSubmit={handleLogin}>
-              {verificationError.length === 0 ? null : <p className="login-input-err">{verificationError}</p>}
-              <input onChange={handleValidation} value={formValue.email} name="email" type="text" placeholder="Email" />
-              <span className="login-input-err">{formError.email}</span>
-              <input onChange={handleValidation} value={formValue.password} name="password" type="password" autoComplete="true" placeholder="Password" />
-              <span className="login-input-err">{formError.password}</span>
-              {submit && Object.keys(formError).length === 0 && !validLogin ?
+          
+            <form>
+              
+              <input onChange={handleChange} value={formData.email} name="email" type="text" placeholder="Email" />
+              {/* <span className="login-input-err">{formError.email}</span> */}
+              <input onChange={handleChange} value={formData.password} name="password" type="password" autoComplete="true" placeholder="Password" />
+              {/* <span className="login-input-err">{formError.password}</span> */}
+
+
+              {/* {submit && Object.keys(formError).length === 0 && !validLogin ?
                 <p className="login-input-err">We couldn't find an account. Try another credentials</p> :
-                null}
+                null} */}
               <section className="login-and-signup">
                 <LinkButton
                   onClick={() => {
@@ -127,13 +81,16 @@ const LoginModal = ({ setLoginModalWindow, setValidLogin, loginModalWindow, hide
                 >
                   Sign up
                 </LinkButton>
-                <button type="submit" className="modal-login-btn">Log in</button>
+                
+              </section>
+              <section className="login-and-signup">
+                <button type="button" className="modal-login-btn" onClick={handleSubmited}>Log in</button>
               </section>
             </form>
-          }
+         
         </section>
       </section>
-    </article>
+    </article></>
   );
 }
 
