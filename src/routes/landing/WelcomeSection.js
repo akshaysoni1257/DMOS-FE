c81b193cf66a8c9017e77df39929ed4fe41147a0
akshaysoni1257 <112935525+akshaysoni1257@users.noreply.github.com>
2024-04-23 12:22:04 +0530

    remove some features

--- src/routes/landing/WelcomeSection.js
+++ src/routes/landing/WelcomeSection.js
@@ -1,9 +1,4 @@
 import React from 'react'
-import { LazyLoadImage } from "react-lazy-load-image-component";
-
-import img375 from '../../assets/images/section-one/section-one-375.webp'
-import img700 from '../../assets/images/section-one/section-one-700.webp'
-import img1440 from '../../assets/images/section-one/section-one-1440.webp'
 import PizzaOne from '../../assets/images/welcome-section/pizza-one-parallax.webp'
 import PizzaTwo from '../../assets/images/welcome-section/pizza-two-parallax.webp'
 import { motion } from "framer-motion";
@@ -43,13 +38,6 @@ const WelcomeSection = () => {
           because you are the one who makes us special!
         </p>
       </section>
-      <LazyLoadImage
-        className="section-two-img"
-        src={img375}
-        srcSet={`${img1440} 1440w, ${img700} 700w, ${img375} 375w`}
-        sizes="(min-width: 1440px) 1440px, (min-width: 700px) 700px, 375px"
-        alt="Pizza Time restaurant interior with people at the tabel and the staff serving the customers"
-      />
     </article>
   )
 }
