5d79046299b7fed73606e55abd5b34be2e537bfd
Jenilambawala <76586953+Jenilambawala@users.noreply.github.com>
2024-04-27 00:05:45 +0530

    Jenil - Order Page Design

--- src/routes/order/orders.js
+++ src/routes/order/orders.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Table from "react-bootstrap/Table";
+
 
 const gettoken = localStorage.getItem("token");
 const Orders = () => {
@@ -21,33 +23,47 @@ const Orders = () => {
 
     return (
         <div className='data-order'>
-            <h2>View Orders</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Order ID</th>
-                        <th>Customer</th>
-                        <th>Product</th>
-                        <th>Quantity</th>
-                        <th>Total Price</th>
-                        <th>Status</th>
-                        <th>Created At</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {orders.map(order => (
-                        <tr key={order._id}>
-                            <td>{order._id}</td>
-                            <td>{order.customer}</td>
-                            <td>{order.items[0].product}</td>
-                            <td>{order.items[0].quantity}</td>
-                            <td>{order.items[0].totalPrice}</td>
-                            <td>{order.status}</td>
-                            <td>{new Date(order.createdAt).toLocaleString()}</td>
+            
+            <div className="cart_table">
+                <div className="order_details">
+                    <h2>Orders Details</h2>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            {/* <th>Order ID</th> */}
+                            <th>Customer Name</th>
+                            <th>Product Name</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Status</th>
+                            {/* <th>Created At</th> */}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {orders.map(order => (
+                            <tr key={order._id}>
+                                {/* <td>{order._id}</td> */}
+                                <td>{order.customer}</td>
+                                <td>{order.items[0].product}</td>
+                                <td>{order.items[0].quantity}</td>
+                                <td>{order.items[0].totalPrice}</td>
+                                <td>{order.status}</td>
+                                {/* <td>{new Date(order.createdAt).toLocaleString()}</td> */}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className="button_wrap">
+                  <a
+                    aria-current="page"
+                    class="cart-btn active-button-style txt-white active"
+                    href="/menu"
+                  >
+                    <p>Back to Menu</p>
+                  </a>
+                </div>
+            </div>
         </div>
     );
 };
